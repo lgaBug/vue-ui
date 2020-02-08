0c36103ca459770174acf1f0868d24f6883b5074
lgaBug <18270234907@163.com>
2020-02-08 22:22:29 +0800

    页面更新

--- src/components/TableMain.vue
+++ src/components/TableMain.vue
@@ -6,20 +6,16 @@
           <div class="layout-logo">IVIEW布局</div>
           <div class="layout-nav">
             <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>
-              Item 1
+              <Icon type="ios-navigate"></Icon>Item 1
             </MenuItem>
             <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>
-              Item 2
+              <Icon type="ios-keypad"></Icon>Item 2
             </MenuItem>
             <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>
-              Item 3
+              <Icon type="ios-analytics"></Icon>Item 3
             </MenuItem>
             <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>
-              Item 4
+              <Icon type="ios-paper"></Icon>Item 4
             </MenuItem>
           </div>
         </Menu>
@@ -30,6 +26,12 @@
           <BreadcrumbItem>Components</BreadcrumbItem>
           <BreadcrumbItem>Layout</BreadcrumbItem>
         </Breadcrumb>
+        <span>用户名：</span>
+        <Input v-model="name" placeholder="Enter something..." clearable style="width: 200px" />
+        <Button type="primary" @click="getPerson">Search</Button>
+
+        <Divider />
+
         <Card>
           <div style="min-height: 200px;">
             <v-table
@@ -48,61 +50,117 @@
 </template>
 
 <script>
-  export default {
-    name: "table-main",
-    data() {
-      return {
-        tableData: [ ],
-        columns: [
-          {field: 'id', title: '序号', width: 80, titleAlign: 'center', columnAlign: 'center',isResize:true},
-          {field: 'createTime', title: '日期', width: 80, titleAlign: 'center', columnAlign: 'center',isResize:true},
-          {field: 'username', title: '姓名', width: 80, titleAlign: 'center', columnAlign: 'center',isResize:true},
-          {field: 'phone', title: '手机号码', width: 150, titleAlign: 'center', columnAlign: 'center',isResize:true},
-          {field: 'email', title: '邮箱', width: 150, titleAlign: 'center', columnAlign: 'center',isResize:true},
-          {field: 'zone', title: '地址', width: 280, titleAlign: 'center', columnAlign: 'center',isResize:true}
-        ]
-      }
-    },
-    created() {
-      //在created函数中使用axios的get请求向后台获取用户信息数据
-      this.$ajax('http://localhost:8889/findAll').then(res => {
-        this.tableData = res.data
+export default {
+  name: "table-main",
+  data() {
+    return {
+      name: "",
+      date: "",
+      tableData: [],
+      columns: [
+        {
+          field: "id",
+          title: "序号",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true
+        },
+        {
+          field: "createTime",
+          title: "日期",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true
+        },
+        {
+          field: "username",
+          title: "姓名",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true
+        },
+        {
+          field: "phone",
+          title: "手机号码",
+          width: 150,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true
+        },
+        {
+          field: "email",
+          title: "邮箱",
+          width: 150,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true
+        },
+        {
+          field: "zone",
+          title: "地址",
+          width: 280,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true
+        }
+      ]
+    };
+  },
+  created() {
+    //在created函数中使用axios的get请求向后台获取用户信息数据
+    this.$ajax("http://localhost:8889/api/findAll")
+      .then(res => {
+        this.tableData = res.data;
         console.log(res.data);
-      }).catch(function (error) {
-        console.log(error);
-      });
+      })
+      .catch(function(error) {});
+  },
+  methods: {
+    getPerson() {
+      this.$ajax("http://localhost:8889/api/person" + "?name=" + this.name)
+        .then(res => {
+          this.tableData = res.data;
+          console.log(res.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
   }
+};
 </script>
 
 <style scoped>
-  .layout{
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-    height: 100%;
-  }
-  .layout-logo{
-    width: 100px;
-    height: 30px;
-    background: #5b6270;
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 15px;
-    left: 20px;
-    font-weight: bold;
-    text-align: center;
-    color: #49ffcc;
-  }
-  .layout-nav{
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
-  }
-  .layout-footer-center{
-    text-align: center;
-  }
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+  height: 100%;
+}
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+  font-weight: bold;
+  text-align: center;
+  color: #49ffcc;
+}
+.layout-nav {
+  width: 420px;
+  margin: 0 auto;
+  margin-right: 20px;
+}
+.layout-footer-center {
+  text-align: center;
+}
 </style>
